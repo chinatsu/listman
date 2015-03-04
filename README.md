f7d7992e68a94f971d54d91b948b3179ee4e395b
chinatsu <lolexplode@gmail.com>
2015-03-04 14:38:22 +0100

    what am i doing

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # listman
 playlist manager to make my coworker's lives easier
+
+## dependencies
+* Python 3
+* PyQT5
+
+## functions
+* it opens files and appends it to the playlist, there's no filter so any file can be added although the mediaplayer might not support it.
+* drag/drop reordering of files
+* delete files, only single selection atm.. kind of impractical
+* saves on exit
+
+## todo
+* move up/down buttons
+* multi selection?
---
+++ manager.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+#from PyQt5.QtGui import *
+import sys, atexit
+
+
+playlist = '/home/china/playlist.m3u'
+
+def loadDialog(listView):
+    fileNames = QFileDialog.getOpenFileNames()
+    if len(fileNames[0]) > 0:
+        listView.addItems(fileNames[0])
+
+def deleteItem(listView):
+    listView.takeItem(listView.currentRow())
+
+def moveItemUp(listView):
+    # TODO
+    currentRow = listView.currentRow()
+    currentItem = listView.takeItem(currentRow)
+    listView.insertItem(currentRow+1, currentItem)
+    
+
+def saveList(listView):
+    with open(playlist, 'w') as f:
+        for item in range(0, listView.count()):
+            f.write(listView.item(item).text() + '\n')
+
+def populateList(listView):
+    with open(playlist, 'r') as f:
+        for line in f:
+            test = line.split('\n')
+            listView.addItem(test[0])
+
+def exitApp(listView):
+    saveList(listView)
+    exit()
+
+def main():
+    app = QApplication(sys.argv)
+    screen = QWidget()
+
+    # widgets
+    loadButton = QPushButton("&Open file", screen)
+    deleteButton = QPushButton("&Delete item", screen)
+#    upButton = QPushButton("", screen)
+#    downButton = QPushButton("", screen)
+    saveButton = QPushButton("&Save", screen)
+    exitButton = QPushButton("&Exit", screen)
+    listView = QListWidget(screen)
+
+    # widget positions
+    loadButton.setGeometry(QRect(10, 10, 100, 32))
+    deleteButton.setGeometry(QRect(120, 10, 100, 32))
+#    upButton.setGeometry(QRect(230, 10, 32, 32))
+#    downButton.setGeometry(QRect(272, 10, 32, 32))
+    saveButton.setGeometry(QRect(10, 460, 100, 32))
+    exitButton.setGeometry(QRect(120, 460, 100, 32))
+    listView.setGeometry(QRect(10, 50, 380, 400))
+
+    # button icons
+#    upButton.setIcon(upButton.style().standardIcon(QStyle.SP_ArrowUp))
+#    downButton.setIcon(downButton.style().standardIcon(QStyle.SP_ArrowDown))
+
+    # app layout
+    appLayout = QGridLayout()
+    appLayout.addWidget(loadButton)
+    appLayout.addWidget(saveButton)
+
+    # events
+    listView.setDragDropMode(QAbstractItemView.InternalMove)
+    loadButton.clicked.connect(lambda: loadDialog(listView))
+#    upButton.clicked.connect(lambda: moveItemUp(listView))
+    deleteButton.clicked.connect(lambda: deleteItem(listView))
+    saveButton.clicked.connect(lambda: saveList(listView))
+    exitButton.clicked.connect(lambda: exitApp(listView))
+    
+
+    # screen setup
+    screen.setWindowTitle('Playlist Manager')
+    screen.resize(400,500)
+    populateList(listView)
+    screen.show()
+    atexit.register(saveList, listView)
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
